1cb6166ae0d20215dba6a853bca5bcb1d42f4e07
Sereymony Ros <sereymonyros@gmail.com>
2025-10-07 05:46:26 +0000

    ?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6238618296-d75b9",
   "appId": "1:1073048556937:web:94e0e79d779916e63a7d58",
+  "storageBucket": "studio-6238618296-d75b9.appspot.com",
   "apiKey": "AIzaSyBQoWVq4ArvPyIKyEOvxtkkbkOShRjtoQs",
   "authDomain": "studio-6238618296-d75b9.firebaseapp.com",
   "measurementId": "",
