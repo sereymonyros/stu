f4441b70b2b48fd7e3f7778d38f3957562cf358d
Sereymony Ros <sereymonyros@gmail.com>
2025-10-09 23:35:32 +0000

    > build > npm run lint && tsc
    > lint
    > eslint --ext .js,.ts .
    
    Oops!

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import { onDocumentCreated } from 'firebase-functions/v2/firestore';
 import * as sgMail from '@sendgrid/mail';
 import { initializeApp } from 'firebase-admin/app';
