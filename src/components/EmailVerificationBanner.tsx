ad45b9246deae1852dcb4a874c5d0842ebd22241
Sereymony Ros <sereymonyros@gmail.com>
2025-10-09 09:22:53 +0000

    what is this? Please use message bar or notification bar

--- src/components/EmailVerificationBanner.tsx
+++ src/components/EmailVerificationBanner.tsx
@@ -37,7 +37,7 @@ export function EmailVerificationBanner() {
 
   if (user && !user.emailVerified) {
     return (
-      <Alert className="rounded-none border-t-0 border-l-0 border-r-0 border-b-primary/50 bg-yellow-50 dark:bg-yellow-950/50 text-foreground">
+      <Alert className="rounded-none border-t-0 border-l-0 border-r-0 border-b-primary/50 bg-primary/10 dark:bg-primary/10 text-foreground">
         <Terminal className="h-4 w-4" />
         <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between w-full">
             <div className="flex-1 mb-2 sm:mb-0">
