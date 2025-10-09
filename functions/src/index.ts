89ebac6aecc32b427552e7b875f0dc45cb5dda08
Sereymony Ros <sereymonyros@gmail.com>
2025-10-09 22:29:29 +0000

    Do step 1, only step 1

---
+++ functions/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * This file is the entry point for your Firebase Functions.
+ * It is automatically discovered and deployed by Firebase.
+ *
+ * You can define HTTP functions, background functions, and more here.
+ *
+ * See the official documentation for more details:
+ * https://firebase.google.com/docs/functions
+ */
+
+// We will add our function logic here in the next steps.
