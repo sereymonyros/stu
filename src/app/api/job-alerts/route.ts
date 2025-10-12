b07eabbeecf9b2a259b13c388e556ea37d0b1145
Sereymony Ros <sereymonyros@gmail.com>
2025-10-12 05:39:08 +0000

    so done for Job Alerts?

--- src/app/api/job-alerts/route.ts
+++
@@ -1,4 +0,0 @@
-
-// This file is intentionally left blank and can be deleted.
-// The cron job functionality was moved to a manual trigger in the dashboard
-// due to development environment restrictions.
