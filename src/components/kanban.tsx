973f1b8003d13f0453d107000ce232ebe4ee9dd5
Sereymony Ros <sereymonyros@gmail.com>
2025-10-13 07:50:25 +0000

    clean up style

--- src/components/kanban.tsx
+++ src/components/kanban.tsx
@@ -289,7 +289,7 @@ function Column({ id, title, children, applicants, isLoading }: { id: string, ti
             <div 
                 ref={setNodeRef} 
                 className={cn(
-                    "p-2 flex-1 rounded-b-lg transition-colors", 
+                    "min-h[125] p-2 flex-1 rounded-b-lg transition-colors", 
                     isOver ? 'bg-primary/10' : 'bg-muted/40'
                 )}
             >
