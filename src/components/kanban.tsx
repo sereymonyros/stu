ae72bef042645378f2b8d591d220675217eb0d79
Sereymony Ros <sereymonyros@gmail.com>
2025-10-13 06:20:30 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@d

--- src/components/kanban.tsx
+++ src/components/kanban.tsx
@@ -4,7 +4,7 @@
 import { useMemo, useState, useEffect } from 'react';
 import { useSortable, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import { useDroppable } from '@d-kit/core';
+import { useDroppable } from '@dnd-kit/core';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
