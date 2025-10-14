9424b3636b53c63bbd599bbbff77d009a04a23f8
Sereymony Ros <sereymonyros@gmail.com>
2025-10-14 03:43:21 +0000

    no scroll by default, only scroll when long content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const heroImage = PlaceHolderImages.find(p => p.id === 'cambodia-hero');
 
   return (
-    <div className="flex flex-col flex-1 h-[calc(100vh-80px)]">
+    <div className="flex flex-col flex-1 min-h-full h-full">
       <div className="relative flex-grow flex flex-col items-center justify-center overflow-hidden p-4 text-center">
         {heroImage && (
           <Image
