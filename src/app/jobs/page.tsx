02a231ad3f2d30caf729358386f2449c843435b1
Sereymony Ros <sereymonyros@gmail.com>
2025-10-16 09:29:11 +0000

    remove background and border color (_for element <LinkComponent>_)

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -626,7 +626,7 @@ function JobsPageContent() {
                              <TooltipProvider>
                                 <Tooltip>
                                     <TooltipTrigger asChild>
-                                        <Button asChild size="icon">
+                                        <Button asChild size="icon" variant="ghost">
                                             <Link href="/jobs/new"><Plus className="h-4 w-4" /></Link>
                                         </Button>
                                     </TooltipTrigger>
