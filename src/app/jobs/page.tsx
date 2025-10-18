1907c25bd614f3f7030cecd0296e095b649b3f3e
Sereymony Ros <sereymonyros@gmail.com>
2025-10-18 07:39:31 +0000

    footer

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -465,7 +465,7 @@ function JobsPageContent() {
 
     return (
         <div className="flex flex-col min-h-screen">
-            <main className="flex-1 p-4 md:p-6 lg:p-8 pb-24">
+            <main className="flex-1 p-4 lg:p-8 pb-6">
                  <div className="mb-6 space-y-4">
                     <div className="flex justify-between items-center">
                          <div className="flex items-center gap-4">
