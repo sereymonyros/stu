88452c1323af837fd0cd0a8c38a898a3c54b33ea
Sereymony Ros <sereymonyros@gmail.com>
2025-10-22 21:32:46 +0000

    bg-background (_for element <Primitive.button>_)

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -8,11 +8,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const toggleVariants = cva(
-  "inline-flex items-center justify-center rounded-3xl text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-foreground data-[state=on]:bg-background",
+  "inline-flex items-center justify-center rounded-3xl text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-foreground data-[state=on]:bg-primary/20",
   {
     variants: {
       variant: {
-        default: "bg-transparent",
+        default: "bg-background",
         outline:
           "border border-input bg-transparent hover:bg-primary/90",
       },
