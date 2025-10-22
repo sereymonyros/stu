d1b4da837421ee4f36898d7b95819e8cb9504757
Sereymony Ros <sereymonyros@gmail.com>
2025-10-22 20:10:35 +0000

    when it is active it should be bg-background (_for element <Primitive.bu

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -8,7 +8,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const toggleVariants = cva(
-  "inline-flex items-center justify-center rounded-3xl text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-red-500",
+  "inline-flex items-center justify-center rounded-3xl text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-foreground data-[state=on]:bg-background",
   {
     variants: {
       variant: {
