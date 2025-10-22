0f14d692954091f766b9de18b0571262f17f005e
Sereymony Ros <sereymonyros@gmail.com>
2025-10-22 21:39:02 +0000

    by default put blue background, click on we make the heart icon become r

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -8,11 +8,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const toggleVariants = cva(
-  "inline-flex items-center justify-center rounded-3xl text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-foreground data-[state=on]:bg-primary/20",
+  "inline-flex items-center justify-center rounded-3xl text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-foreground data-[state=on]:bg-primary/30",
   {
     variants: {
       variant: {
-        default: "bg-background",
+        default: "bg-primary/20",
         outline:
           "border border-input bg-transparent hover:bg-primary/90",
       },
