f9ed2a178ff263c779f6e6493abceb42a0d411c9
Sereymony Ros <sereymonyros@gmail.com>
2025-10-22 22:16:38 +0000

    by default viewmode = list, so the board button should not have backgrou

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -8,13 +8,13 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const toggleVariants = cva(
-  "inline-flex items-center justify-center rounded-3xl text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-foreground data-[state=on]:bg-primary/30",
+  "inline-flex items-center justify-center rounded-3xl text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-foreground data-[state=on]:bg-primary/20",
   {
     variants: {
       variant: {
-        default: "bg-primary/20",
+        default: "bg-transparent",
         outline:
-          "border border-input bg-transparent hover:bg-primary/90",
+          "border border-input bg-background hover:bg-primary/90",
       },
       size: {
         default: "h-10 px-3",
