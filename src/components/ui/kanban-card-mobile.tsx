22d1b9b21a860d603c1ee9ca0f86e6db0c8d5605
Sereymony Ros <sereymonyros@gmail.com>
2025-10-23 22:12:06 +0000

    remove a link, so it is not clickable, so it is not a link to /detail (_

--- src/components/ui/kanban-card-mobile.tsx
+++ src/components/ui/kanban-card-mobile.tsx
@@ -1,9 +1,7 @@
 
 'use client';
 
-import Link from 'next/link';
 import { Card } from '@/components/ui/card';
-import { Badge } from '@/components/ui/badge';
 import { cn } from '@/lib/utils';
 import { ApplicantCounter } from '../applicant-counter';
 
@@ -13,16 +11,15 @@ export function KanbanCardMobile({
     job: any;
 }) {
     return  (
-        <Link href={`/jobs/${job.id}/details`} className="block group/card">
+        <div className="block group/card">
             <Card className={cn("hover:shadow-md transition-shadow duration-200 w-full relative group/item rounded-2xl flex justify-between")}>
-                <div className="flex-1 py-2 pl-2 min-w-0">
+                <div className="flex-1 py-2 pl-2 pr-4 min-w-0">
                     <p className="text-xs font-medium truncate">{job.title}</p>
                 </div>
                 <div className="flex-shrink-0">
                     <ApplicantCounter jobId={job.id} layout="vertical" />
                 </div>
             </Card>
-        </Link>
+        </div>
     );
 }
-
