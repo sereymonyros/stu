bfe985359f919fcc8678116b03ea8894c8aaa183
Sereymony Ros <sereymonyros@gmail.com>
2025-10-23 22:13:23 +0000

    make it no select, no selectable. So it will be easy to drag and drop (_

--- src/components/ui/kanban-card-mobile.tsx
+++ src/components/ui/kanban-card-mobile.tsx
@@ -13,7 +13,7 @@ export function KanbanCardMobile({
     return  (
         <div className="block group/card">
             <Card className={cn("hover:shadow-md transition-shadow duration-200 w-full relative group/item rounded-2xl flex justify-between")}>
-                <div className="flex-1 py-2 pl-2 pr-4 min-w-0">
+                <div className="flex-1 py-2 pl-2 pr-4 min-w-0 select-none">
                     <p className="text-xs font-medium truncate">{job.title}</p>
                 </div>
                 <div className="flex-shrink-0">
