0f8a55b2042beed1a1b03a1f0c5d8dae4a9438ab
Sereymony Ros <sereymonyros@gmail.com>
2025-10-25 17:57:56 +0000

    deploy to prod, and here is what i get 8718-0aeaf52e03420c06.js:1  Unca

---
+++ src/firebase/config.ts
@@ -0,0 +1,19 @@
+// This file contains the public-facing Firebase configuration.
+// It is used to initialize the Firebase client-side SDK.
+
+// How to get these values:
+// 1. Go to your Firebase project in the Firebase Console.
+// 2. Click the gear icon > Project settings.
+// 3. In the "General" tab, scroll down to "Your apps".
+// 4. Select your web app.
+// 5. Choose "Config" to see the firebaseConfig object.
+// 6. Copy the values into the object below.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQoWVq4ArvPyIKyEOvxtkkbkOShRjtoQs",
+  authDomain: "studio-6238618296-d75b9.firebaseapp.com",
+  projectId: "studio-6238618296-d75b9",
+  storageBucket: "studio-6238618296-d75b9.appspot.com",
+  messagingSenderId: "1073048556937",
+  appId: "1:1073048556937:web:94e0e79d779916e63a7d58",
+};
